a8e055fa456e66f98e5cb213351bed739fa7ff20
Don1L <danilic206@gmail.com>
2026-05-31 22:16:25 +0300

    feat(web): add PublicationServlet with list, create, update, sell, delete

--- news-kiosk/src/main/java/com/kiosk/model/Publication.java
+++ news-kiosk/src/main/java/com/kiosk/model/Publication.java
@@ -18,6 +18,8 @@ public abstract class Publication {
     public double getPrice() { return price; }
     public int getQuantity() { return quantity; }
 
+    public abstract String getType();
+
     public void setId(long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
     public void setPrice(double price) { this.price = price; }
