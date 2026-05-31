6490001bba50e393f039aa926e2cee307fb6f677
Don1L <danilic206@gmail.com>
2026-05-31 23:14:28 +0300

    fix(main): close Statement resource after setting client encoding

--- news-kiosk/src/main/java/com/kiosk/Main.java
+++ news-kiosk/src/main/java/com/kiosk/Main.java
@@ -26,7 +26,9 @@ public class Main {
                 props.getProperty("db.user"),
                 props.getProperty("db.password"))) {
 
-            connection.createStatement().execute("SET client_encoding TO 'UTF8'");
+            try (java.sql.Statement st = connection.createStatement()) {
+                st.execute("SET client_encoding TO 'UTF8'");
+            }
             PublicationRepository repository = new JdbcPublicationRepository(connection);
             PublicationService service = new PublicationServiceImpl(repository);
             ConsoleUI ui = new ConsoleUI(service);
