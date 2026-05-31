e81d15fb1d10db695bb943ff9205609c5d92a613
Don1L <danilic206@gmail.com>
2026-05-31 21:28:40 +0300

    refactor(main): switch to JdbcPublicationRepository

--- news-kiosk/src/main/java/com/kiosk/Main.java
+++ news-kiosk/src/main/java/com/kiosk/Main.java
@@ -1,16 +1,35 @@
 package com.kiosk;
 
 import com.kiosk.repository.PublicationRepository;
-import com.kiosk.repository.inmemory.InMemoryPublicationRepository;
+import com.kiosk.repository.jdbc.JdbcPublicationRepository;
 import com.kiosk.service.PublicationService;
 import com.kiosk.service.impl.PublicationServiceImpl;
 import com.kiosk.ui.ConsoleUI;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        PublicationRepository repository = new InMemoryPublicationRepository();
-        PublicationService service = new PublicationServiceImpl(repository);
-        ConsoleUI ui = new ConsoleUI(service);
-        ui.start();
+
+    public static void main(String[] args) throws IOException, SQLException {
+        Properties props = new Properties();
+        try (InputStream is = Main.class.getClassLoader().getResourceAsStream("db.properties")) {
+            props.load(is);
+        }
+
+        try (Connection connection = DriverManager.getConnection(
+                props.getProperty("db.url"),
+                props.getProperty("db.user"),
+                props.getProperty("db.password"))) {
+
+            PublicationRepository repository = new JdbcPublicationRepository(connection);
+            PublicationService service = new PublicationServiceImpl(repository);
+            ConsoleUI ui = new ConsoleUI(service);
+            ui.start();
+        }
     }
 }
